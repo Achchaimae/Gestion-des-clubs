f55628ff5cec0f713ea99dc06f72b13fba64c1c3
achchaimae <achchaimae1@gmail.com>
2022-11-24 14:51:25 +0100

    modify database

--- d/fichier sql/gestion.sql
+++ d/fichier sql/gestion.sql
@@ -6,18 +6,9 @@ id int not null auto_increment,
 primary key(id),
 nom tinytext,
 membres int,
-logo blob,
+logo varchar(1000),
 date_creation  date,
 description text
-);
-
-create table apprenant(
-id_apprenant int not null auto_increment,
-primary key(id_apprenant),
-nom_complet tinytext,
-class tinytext,
-age int,
-email tinytext
 );
 
 create table evenement (
@@ -30,28 +21,36 @@ lieu tinytext,
 description text
 );
 
-create table membre(
-id_membre int not null,
-foreign key (id_membre) references apprenant(id_apprenant),
- role tinytext,
- id_club int not null,
-foreign key (id_club) references club(id)
-);
-
-create table amin(
+create table admin(
 id int not null auto_increment primary key,
-login tinytext,
+login varchar(10),
 password text
 );
 create table demande(
-id_demande int not null auto_increment primary key,
-id_apprenant int ,
-foreign key (id_apprenant) references apprenant(id_apprenant),
-state boolean,
-club_integre int 
+id_demande int not null auto_increment,
+primary key(id_demande),
+nom_complet tinytext,
+class tinytext,
+age int,
+email tinytext,
+id_club int,
+foreign key (id_club) references club(id)
 );
+
 create table ToDo(
 id int not null auto_increment primary key,
 task text
 );
+create table membre(
+id_membre int not null auto_increment,
+primary key(id_membre),
+nom_complet tinytext,
+class tinytext,
+age int,
+email tinytext,
+membre_role tinytext,
+ id_club int not null,
+foreign key (id_club) references club(id)
+);
+
 
