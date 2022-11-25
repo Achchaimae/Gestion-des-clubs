70eab48cc473923d7e38a7ce5d8b260f46c2e6ee
abdallah <abddoullah.bouzhar@gmail.com>
2022-11-25 09:10:50 +0100

    gestion2

--- d/fichier sql/gestion.sql
+++ d/fichier sql/gestion.sql
@@ -1,59 +1,58 @@
-create database gestion;
-use gestion;
+create database gestion2;
+use gestion2;
 
 create table club(
 id int not null auto_increment,
 primary key(id),
-nom tinytext,
+nom varchar(20),
 membres int,
 logo blob,
 date_creation  date,
-description text
-);
-
-create table apprenant(
-id_apprenant int not null auto_increment,
-primary key(id_apprenant),
-nom_complet tinytext,
-class tinytext,
-age int,
-email tinytext
+description text,
+rep int
 );
 
 create table evenement (
 id int not null auto_increment primary key,
 id_club int not null,
-foreign key (id_club) references club(id) ON DELETE CASCADE  Or 
-On delete set  null,
-nom tinytext,
+foreign key (id_club) references club(id),
+nom varchar(20),
 event_date date,
-lieu tinytext,
+lieu varchar(20),
 description text
 );
 
-create table membre(
-id_membre int not null,
-foreign key (id_membre) references apprenant(id_apprenant),
- role tinytext,
- id_club int not null,
-foreign key (id_club) references club(id) ON DELETE CASCADE  Or 
-On delete set  null
-);
-
-create table amin(
+create table admin(
 id int not null auto_increment primary key,
-login tinytext,
+login varchar(20),
 password text
 );
 create table demande(
-id_demande int not null auto_increment primary key,
-id_apprenant int ,
-foreign key (id_apprenant) references apprenant(id_apprenant),
-state boolean,
-club_integre int 
+id_demande int not null auto_increment,
+primary key(id_demande),
+nom_complet varchar(20),
+class varchar(20),
+age int,
+email varchar(20),
+id_club int,
+foreign key (id_club) references club(id)
 );
+
 create table ToDo(
 id int not null auto_increment primary key,
 task text
 );
+
+create table membre(
+id_membre int not null auto_increment,
+primary key(id_membre),
+nom_complet varchar(20),
+class varchar(20),
+age int,
+email varchar(20),
+membre_role varchar(20),
+id_club int ,
+foreign key (id_club) references club(id) ON DELETE SET NULL
+);
+
 
