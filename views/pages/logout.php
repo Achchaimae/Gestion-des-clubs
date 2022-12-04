2afe3a6e6090a268c24c4d460c44b2f4bf017aa8
abdallah <abddoullah.bouzhar@gmail.com>
2022-12-05 00:13:25 +0100

    frech

--- views/pages/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-unset($_SESSION['login']);
-session_destroy();
-header('Location: index.php?c');
--- views/todos/list.php
+++
@@ -1,9 +0,0 @@
-
-<?php  foreach($listclubs as $key=>$value){ ?>
-                <li class="task">
-                        <p class="task-test"> <?php echo $value["task"]; ?></p>
-
-                        <a href="?c=Todo&a=delete&id=<?php echo $value["id"];?>"> <i class="fa-solid fa-xmark"></i></a>
-                </li>
-<?php  }
-?>
